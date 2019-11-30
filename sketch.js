a12f755ca5c5c7c090b1b4da2d943be182f1f0dc
Maximilien Mellen <maxmellen0@gmail.com>
2019-11-30 23:55:08 +0100

    React to music

--- sketch.js
+++ sketch.js
@@ -1,30 +1,44 @@
 "use strict";
 
+let root2;
+
 let d = 0;
+let sound;
+let fft;
+let spectrum;
+
+function preload() {
+  sound = loadSound("./track.mp3");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  colorMode(HSB, 360);
+  fft = new p5.FFT(0);
+  colorMode(HSB);
   angleMode(DEGREES);
   rectMode(CENTER);
   noFill();
+  root2 = sqrt(2);
 }
 
 function draw() {
   const S = min(width, height);
+  strokeWeight(S / 360);
   background(0);
+  spectrum = fft.analyze();
   translate(width / 2, height / 2);
-  for (let a = 0; a < 360 * 4; a++) {
+  for (let a = 0; a < 360 * 6; a++) {
+    const f = spectrum[round(a * 1024 / 360) % 360] / 255;
     const x = cos(a) * S / 4;
     const y = sin(a) * S / 4;
-    const s = S / 5;
+    const s = S / 2 / root2 * pow(f, 3);
     const h = a;
 
     push();
     {
       translate(x, y);
       rotate(a + d);
-      stroke((360 + h - d) % 360, 360, 360, 30);
+      stroke((360 + h - d) % 360, 100, 100, 5 * (1 - pow(f, 1/3)) / 6 + 1 / 6);
       square(0, 0, s);
     }
     pop();
@@ -37,5 +51,14 @@ function windowResized() {
 }
 
 function mousePressed() {
-  noLoop();
+  console.log(spectrum);
+  togglePlay();
+}
+
+function togglePlay() {
+  if (sound.isPlaying()) {
+    sound.pause();
+  } else {
+    sound.loop();
+  }
 }
